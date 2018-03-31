f3c8789227ce09ad942f6fff49073f03de23d48f
JoAn <joannacalinska@gmail.com>
2018-03-31 22:04:46 +0200

    Node-day1-part2

--- app/zadanieDnia.js
+++ app/zadanieDnia.js
@@ -1,1 +1,9 @@
-//Twój kod
+// for (let i=2; i<process.argv.length; i++) {
+//     setTimeout(() => {
+//         console.log(process.argv[i]);
+//     }, Number(process.argv[i])*1000)
+// }
+
+const args = process.argv.slice(2);
+
+args.forEach(arg => setTimeout(() => {console.log(arg);}, Number(arg)*1000));
